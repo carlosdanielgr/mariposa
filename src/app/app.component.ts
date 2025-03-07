75d739c7eabfab9db775e8cff48d49fc92edef0c
carlosdanielgr <carlos.dgr.12@gmail.com>
2025-03-06 22:26:43 -0500

    great delivery partially completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,16 @@ import { Component } from '@angular/core';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { GreatDeliveryComponent } from './components/great-delivery/great-delivery.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, MainComponent, FooterComponent],
+  imports: [
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    GreatDeliveryComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/components/great-delivery/great-delivery.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-great-delivery',
+  imports: [],
+  templateUrl: './great-delivery.component.html',
+  styleUrl: './great-delivery.component.scss'
+})
+export class GreatDeliveryComponent {
+
+}
