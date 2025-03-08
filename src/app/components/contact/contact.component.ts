7c079bef3a9ca04066b215d082a69e7f608dc1be
carlosdanielgr <carlos.dgr.12@gmail.com>
2025-03-08 12:41:39 -0500

    form component partially completed

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-contact',
-  imports: [FooterComponent],
+  imports: [FooterComponent, FormComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
 })
