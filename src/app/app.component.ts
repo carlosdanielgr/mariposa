c63bb004bcfd30425ab719afeb88955254519482
carlosdanielgr <carlos.dgr.12@gmail.com>
2025-03-07 23:12:34 -0500

    contact component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { GreatDeliveryComponent } from './components/great-delivery/great-delivery.component';
 import { FeatureListComponent } from './components/feature-list/feature-list.component';
 import { WhyChooseComponent } from './components/why-choose/why-choose.component';
 import { ColaborateComponent } from './components/colaborate/colaborate.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
     MainComponent,
-    FooterComponent,
     GreatDeliveryComponent,
     FeatureListComponent,
     WhyChooseComponent,
     ColaborateComponent,
+    ContactComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
