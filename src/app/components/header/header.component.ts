36d8b421f40992428186e661d1bb1a707d02507a
carlosdanielgr <carlos.dgr.12@gmail.com>
2025-03-08 14:32:00 -0500

    fix mobile completed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isVisible: boolean = true;
+  lastScrollTop: number = 0;
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const currentScroll = window.scrollX || document.documentElement.scrollTop;
 
+    if (currentScroll > this.lastScrollTop) {
+      this.isVisible = false;
+    } else {
+      this.isVisible = true;
+    }
+
+    this.lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  }
 }
